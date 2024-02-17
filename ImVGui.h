0987fc2080927f6a15e3285a177082e6e0e3edd3
K J <anticream39@yahoo.com>
2024-02-16 21:07:20 -0500

    Create ImVGui.h

---
+++ ImVGui.h
@@ -0,0 +1,14 @@
+/*
+  ImVGui
+
+  A collection of Vgui styled controls for ImGui, supports multiple languages
+
+  (C) 2024 K. 'ashi/eden' J.
+*/
+#prgama once
+
+#include <imgui.h>
+
+namespace ImVGui {
+  // todo: convert dotnet code to C++
+}; // namespace ImVGui
